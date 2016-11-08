8a5d622f1a0ff90e578f508109ae0c4b3d6cd389
Jacob Wagner <swagner1104@gmail.com>
2016-11-08 11:19:37 -0600

    added update stack basic script and updated the template to allow update to run smoothly and consistantly
    Closes https://github.com/rcbops-qe/rpc-heat-ansible/issues/14

--- scripts/update_stack.sh
+++ scripts/update_stack.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+# This script will update a heat stack for rpc-on-rpc
+
+set -ex
+
+export STACK_NAME=${STACK_NAME:-"test_stack"}
+
+openstack stack update -e environments/user-rpc-on-rpc-13.1-full.yml -t templates/rpc-on-rpc-full.yml --timeout 240 --wait $STACK_NAME
